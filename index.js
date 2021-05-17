a8984260e5b24aafa807c26550b34666c0b509e4
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-05-17 11:48:11 +0400

    .csv headind fixed

--- index.js
+++ index.js
@@ -1,51 +1,9 @@
-// todo: select directory with readline ?
+const fs = require("fs");
+const rp2csv = require("./lib/recursive-pcm2csv");
 
-/** .PCM pole 
- * ? [Object].Etude.Supports[0].Support[i] --> there is only one value in Supports array
-{
-  * ! Nom: [ 'NC0001' ],
-  Facade: [ '0' ],
-  Nature: [ 'BE' ],
-  * ! Hauteur: [ '11' ], 
-  * ! Classe: [ 'A' ],
-  * ! Effort: [ '1,5' ],
-  Orientation: [ '390' ],
-  Etat: [ 'Bon �tat' ],
-  Commentaire: [ '' ],
-  BranchementsBT: [ '0' ],
-  optBranchementsTelExistants: [ '0' ],
-  * ! optBandeauVertAPoser: [ '0' ],
-  optBandeauVertExistant: [ '0' ],
-  ReservationEP: [ '0' ],
-  PresenceEP: [ '1' ],
-  HauteurEP: [ '9' ],
-  APoser: [ '0' ],
-  * ! NonCalcule: [ '1' ], 
-  Illisible: [ '1' ],
-  Surimplantation: [ '0' ],
-  * ! X: [ '444571,2' ],
-  * ! Y: [ '6430938' ],
-  Z: [ '0' ],
-  Annee: [ '' ],
-  optMALTBT: [ '0' ],
-  RASBT: [ '0' ],
-  RASFT: [ '0' ],
-  RASCO: [ '0' ],
-  RASFO: [ '0' ],
-  optMALTCuivre: [ '0' ],
-  optMALTCoaxial: [ '0' ],
-  optBoitierCuivre: [ '0' ],
-  optBoitierCoaxial: [ '0' ],
-  optBoitierFibre: [ '0' ],
-  NbRaccordementsCuivre: [ '0' ],
-  NbRaccordementsCoaxial: [ '0' ],
-  NbRaccordementsFibre: [ '0' ]
-  */
-
-/** BETTER COMMENTS METHOD
- * *important
- * !très important
- * ?question
- * todo:a faire
- * @param parametre
- */
+try {
+  fs.appendFileSync("./waste/comac.csv", rp2csv("/Volumes/Windows/00-Engie"));
+  console.log("File successfully created !");
+} catch (error) {
+  console.log(error.message);
+}
